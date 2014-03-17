3726ad16e2f2451f0684ee3bcada92ac96286987
Amy Leikert <amy.leikert@detroitlabs.com>
2014-03-17 13:51:25 -0400

    Initial Commit

---
+++ IceCreamShop/GCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GCAppDelegate.h
+//  IceCreamShop
+//
+//  Created by DetroitLabsUser on 3/17/14.
+//  Copyright (c) 2014 DetroitLabsUser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IceCreamShop/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IceCreamShop
+//
+//  Created by DetroitLabsUser on 3/17/14.
+//  Copyright (c) 2014 DetroitLabsUser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GCAppDelegate class]));
+    }
+}
